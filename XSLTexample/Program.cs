ed0cd703e545f2f26c4287c894eba07e7b02b5fa
Nikita Gromyko <n.gromyko@itransition.com>
2020-09-21 18:14:46 +0300

    Add project files.

---
+++ XSLTexample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Xsl;
+
+namespace XSLTexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load("transform.xsl");
+
+            xslt.Transform("books.xml", "books.html");
+        }
+    }
+}
